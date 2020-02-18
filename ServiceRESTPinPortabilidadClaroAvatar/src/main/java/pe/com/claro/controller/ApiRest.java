b3cc91dce627d5035b205a59f0b29a68df259653
jcisneros <jcisneros@avatar-global.com>
2020-02-18 15:41:31 -0500

    jcisneros_180220: limpieza de codigo

--- ServiceRESTPinPortabilidadClaroAvatar/src/main/java/pe/com/claro/controller/ApiRest.java
+++ ServiceRESTPinPortabilidadClaroAvatar/src/main/java/pe/com/claro/controller/ApiRest.java
@@ -66,7 +66,7 @@ public class ApiRest {
 	@PostMapping(value = "/generarCodigo", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Response> generarCodigo(@RequestBody Request request) {
 		String METHOD_NAME = "Capa RestController  - {generarCodigo} ";
-		log.info(METHOD_NAME + "Inicia API REST.");
+		log.info(METHOD_NAME , "Inicia API REST.");
 		Response response = new Response();
 		HttpStatus estadoHttp = HttpStatus.CREATED;
 		RespuestaDomain codigoSMSGenerado = new RespuestaDomain();
@@ -86,7 +86,7 @@ public class ApiRest {
 
 		HeaderRequestGenerarCodigo headerRequest = new HeaderRequestGenerarCodigo();
 		BodyRequestGenerarCodigo bodyRequest = new BodyRequestGenerarCodigo();
-		log.info(METHOD_NAME + "Se valida la data ingresada en el REQUEST.");
+		log.info(METHOD_NAME , "Se valida la data ingresada en el REQUEST.");
 		if (request == null || request.getMessageRequest() == null || request.getMessageRequest().getHeader() == null
 				|| request.getMessageRequest().getHeader().getHeaderRequest() == null
 				|| request.getMessageRequest().getBody() == null) {
@@ -104,7 +104,7 @@ public class ApiRest {
 					BodyRequestGenerarCodigo.class);
 
 		} catch (IOException e) {
-			log.error(constante.IDT1_GENERAR_CODIGO_ERROR() + e.getMessage());
+			log.error(constante.IDT1_GENERAR_CODIGO_ERROR() , e.getMessage());
 			bodyResponse.setCodigoRespuesta(Constante.CODIGO.IDT1_GENERAR_CODIGO_ERROR);
 			bodyResponse.setMensajeRespuesta(constante.IDT1_GENERAR_CODIGO_ERROR());
 			messageResponse.setHeader(headerResponse);
@@ -112,7 +112,7 @@ public class ApiRest {
 			response.setMessageResponse(messageResponse);
 			estadoHttp = HttpStatus.INTERNAL_SERVER_ERROR;
 		}
-		log.info(METHOD_NAME + "Se valida la data ingresada en el HEADER del request.");
+		log.info(METHOD_NAME , "Se valida la data ingresada en el HEADER del request.");
 		if (headerRequest == null) {
 			headerResponse.setHeaderResponse(headerResponseGenerarCodigo);
 			bodyResponse.setCodigoRespuesta(Constante.CODIGO.IDF2_GENERAR_CODIGO_NO_HEADER_CORRECTO);
@@ -122,7 +122,7 @@ public class ApiRest {
 			response.setMessageResponse(messageResponse);
 			return new ResponseEntity<>(response, estadoHttp);
 		}
-		log.info(METHOD_NAME + "Se valida la data ingresada en el BODY del request.");
+		log.info(METHOD_NAME , "Se valida la data ingresada en el BODY del request.");
 		if (bodyRequest == null || StringUtils.isBlank(bodyRequest.getFlagEnvioSMS())
 				|| StringUtils.isBlank(bodyRequest.getFlagSMSIndico()) || StringUtils.isBlank(bodyRequest.getLinea())
 				|| StringUtils.isBlank(bodyRequest.getNumeroDocumento())) {
@@ -139,21 +139,21 @@ public class ApiRest {
 			/** Se valida que el flagEnvioSMS = 1 */
 			if (Constante.PARAMETRO.FLAG_ENVIO_SMS.equalsIgnoreCase(bodyRequest.getFlagEnvioSMS())) {
 				esFlagEnvioSMS = true;
-				log.info(METHOD_NAME + "Se valido con exitoso que el flagEnvioSMS = 1.");
+				log.info(METHOD_NAME , "Se valido con exitoso que el flagEnvioSMS = 1.");
 
 				/** Se valida el numero de intentos */
 				rptaValidarIntentos = solicitudService.validarIntentos(bodyRequest);
 				if (Constante.PARAMETRO.GENERAR_CODIGO_VALIDAR_INTENTOS_EXITO
 						.equalsIgnoreCase(rptaValidarIntentos.getCodigo())) {
 					seValidoNroIntentos = true;
-					log.info(METHOD_NAME + "Se valido con exitoso el numero de intentos.");
+					log.info(METHOD_NAME , "Se valido con exitoso el numero de intentos.");
 
 					/** Se valida si existe el cliente */
 					Cliente cliente = clienteService.encontrarXNumDoc(bodyRequest.getNumeroDocumento());
 					if (cliente != null) {
 						seValidoExisteCliente = true;
-						log.info(METHOD_NAME + "Se valido con exitoso que el cliente: " + cliente.getNombre()
-								+ " con nro de documento: " + cliente.getNumDoc() + " exista.");
+						log.info(METHOD_NAME , "Se valido con exitoso que el cliente: " , cliente.getNombre()
+								, " con nro de documento: " , cliente.getNumDoc() , " exista.");
 
 						/** Se genera el codigo SMS */
 						codigoSMSGenerado = solicitudService.generarCodigo(bodyRequest, cliente);
@@ -161,8 +161,8 @@ public class ApiRest {
 								.equalsIgnoreCase(codigoSMSGenerado.getCodigo())) {
 							seGeneroCodigoSMS = true;
 							solicitudCreada = (Solicitud) codigoSMSGenerado.getObjeto();
-							log.info(METHOD_NAME + "Se genero el siguiente token: "+solicitudCreada.getToken());
-							log.info(METHOD_NAME + "Se genero el siguiente codigo SMS: "+solicitudCreada.getCodigoSMS());
+							log.info(METHOD_NAME , "Se genero el siguiente token: "+solicitudCreada.getToken());
+							log.info(METHOD_NAME , "Se genero el siguiente codigo SMS: "+solicitudCreada.getCodigoSMS());
 
 							/** Se valida que el flagEnvioSMSIndico = 1 */
 							RespuestaDomain rptaEnvioCodigoSMS = new RespuestaDomain();
@@ -205,35 +205,35 @@ public class ApiRest {
 				data.setListaOpcionales(lstOpcionales);
 				bodyResponse.setData(data);
 				estadoHttp = HttpStatus.OK;
-				log.info(METHOD_NAME + bodyResponse.getMensajeRespuesta());
+				log.info(METHOD_NAME , bodyResponse.getMensajeRespuesta());
 			} else {
 				if (!esFlagEnvioSMS) {
 					bodyResponse.setCodigoRespuesta(Constante.CODIGO.IDF4_GENERAR_CODIGO_NO_FLAG_ENVIO_SMS);
 					bodyResponse.setMensajeRespuesta(constante.IDF4_GENERAR_CODIGO_NO_FLAG_ENVIO_SMS());
-					log.info(METHOD_NAME + bodyResponse.getMensajeRespuesta());
+					log.info(METHOD_NAME , bodyResponse.getMensajeRespuesta());
 				} else if (!seValidoNroIntentos) {
 					bodyResponse.setCodigoRespuesta(Constante.CODIGO.IDF1_GENERAR_CODIGO_NO_VALIDO_NRO_INTENTOS);
 					bodyResponse.setMensajeRespuesta(constante.IDF1_GENERAR_CODIGO_NO_VALIDO_NRO_INTENTOS());
-					log.info(METHOD_NAME + bodyResponse.getMensajeRespuesta());
+					log.info(METHOD_NAME , bodyResponse.getMensajeRespuesta());
 				} else if (!seValidoExisteCliente) {
 					bodyResponse.setCodigoRespuesta(Constante.CODIGO.IDF6_GENERAR_CODIGO_NO_EXISTE_CLIENTE);
 					bodyResponse.setMensajeRespuesta(constante.IDF6_GENERAR_CODIGO_NO_EXISTE_CLIENTE());
-					log.info(METHOD_NAME + bodyResponse.getMensajeRespuesta());
+					log.info(METHOD_NAME , bodyResponse.getMensajeRespuesta());
 				} else if (!seGeneroCodigoSMS) {
 					bodyResponse.setCodigoRespuesta(Constante.CODIGO.IDF7_GENERAR_CODIGO_NO_GENERO_CODIGO_SMS);
 					bodyResponse.setMensajeRespuesta(constante.IDF7_GENERAR_CODIGO_NO_GENERO_CODIGO_SMS());
-					log.info(METHOD_NAME + bodyResponse.getMensajeRespuesta());
+					log.info(METHOD_NAME , bodyResponse.getMensajeRespuesta());
 				} else if (!SeEnvioSMS) {
 					bodyResponse.setCodigoRespuesta(Constante.CODIGO.IDF5_GENERAR_CODIGO_NO_ENVIO_SMS);
 					bodyResponse.setMensajeRespuesta(constante.IDF5_GENERAR_CODIGO_NO_ENVIO_SMS());
-					log.info(METHOD_NAME + bodyResponse.getMensajeRespuesta());
+					log.info(METHOD_NAME , bodyResponse.getMensajeRespuesta());
 				}
 				estadoHttp = HttpStatus.CREATED;
 			}
-			log.info(METHOD_NAME + "Termina API REST.");
+			log.info(METHOD_NAME , "Termina API REST.");
 
 		} catch (Exception e) {
-			log.error(constante.IDT1_GENERAR_CODIGO_ERROR() + e.getMessage());
+			log.error(constante.IDT1_GENERAR_CODIGO_ERROR() , e.getMessage());
 			bodyResponse.setCodigoRespuesta(Constante.CODIGO.IDT1_GENERAR_CODIGO_ERROR);
 			bodyResponse.setMensajeRespuesta(constante.IDT1_GENERAR_CODIGO_ERROR());
 			estadoHttp = HttpStatus.INTERNAL_SERVER_ERROR;
@@ -248,7 +248,7 @@ public class ApiRest {
 	@PostMapping(value = "/validarCodigo", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Response> validarCodigo(@RequestBody Request request) {
 		String METHOD_NAME = "Capa RestController - {validarCodigo} ";
-		log.info(METHOD_NAME + "Inicia API REST.");
+		log.info(METHOD_NAME , "Inicia API REST.");
 		Response response = new Response();
 		HttpStatus estadoHttp = HttpStatus.CREATED;
 		RespuestaDomain codigoValidado = new RespuestaDomain();
@@ -260,7 +260,7 @@ public class ApiRest {
 
 		HeaderRequestValidarCodigo headerRequest = new HeaderRequestValidarCodigo();
 		BodyRequestValidarCodigo bodyRequest = new BodyRequestValidarCodigo();
-		log.info(METHOD_NAME + "Se valida la data ingresada en el REQUEST.");
+		log.info(METHOD_NAME , "Se valida la data ingresada en el REQUEST.");
 		if (request == null || request.getMessageRequest() == null || request.getMessageRequest().getHeader() == null
 				|| request.getMessageRequest().getHeader().getHeaderRequest() == null
 				|| request.getMessageRequest().getBody() == null) {
@@ -278,7 +278,7 @@ public class ApiRest {
 					BodyRequestValidarCodigo.class);
 
 		} catch (IOException e) {
-			log.error(constante.IDT1_GENERAR_CODIGO_ERROR() + e.getMessage());
+			log.error(constante.IDT1_GENERAR_CODIGO_ERROR() , e.getMessage());
 			bodyResponse.setCodigoRespuesta(Constante.CODIGO.IDT1_VALIDAR_CODIGO_ERROR);
 			bodyResponse.setMensajeRespuesta(constante.IDT1_VALIDAR_CODIGO_ERROR());
 			messageResponse.setHeader(headerResponse);
@@ -286,7 +286,7 @@ public class ApiRest {
 			response.setMessageResponse(messageResponse);
 			estadoHttp = HttpStatus.INTERNAL_SERVER_ERROR;
 		}
-		log.info(METHOD_NAME + "Se valida la data ingresada en el HEADER del request.");
+		log.info(METHOD_NAME , "Se valida la data ingresada en el HEADER del request.");
 		if (headerRequest == null) {
 			headerResponse.setHeaderResponse(headerResponseGenerarCodigo);
 			bodyResponse.setCodigoRespuesta(Constante.CODIGO.IDF2_VALIDAR_CODIGO_NO_HEADER_CORRECTO);
@@ -296,7 +296,7 @@ public class ApiRest {
 			response.setMessageResponse(messageResponse);
 			return new ResponseEntity<>(response, estadoHttp);
 		}
-		log.info(METHOD_NAME + "Se valida la data ingresada en el BODY del request.");
+		log.info(METHOD_NAME , "Se valida la data ingresada en el BODY del request.");
 		if (bodyRequest == null || StringUtils.isBlank(bodyRequest.getCodigo())
 				|| StringUtils.isBlank(bodyRequest.getEnvioSMS())) {
 			headerResponse.setHeaderResponse(headerResponseGenerarCodigo);
@@ -314,27 +314,27 @@ public class ApiRest {
 				bodyResponse.setCodigoRespuesta(codigoValidado.getCodigo());
 				bodyResponse.setMensajeRespuesta(constante.IDF0_VALIDAR_CODIGO_EXITO());
 				estadoHttp = HttpStatus.OK;
-				log.info(METHOD_NAME + bodyResponse.getMensajeRespuesta());
+				log.info(METHOD_NAME , bodyResponse.getMensajeRespuesta());
 			} else if (Constante.CODIGO.IDF1_VALIDAR_CODIGO_CANTIDAD_INTENTOS_SUPERADO
 					.equalsIgnoreCase(codigoValidado.getCodigo())) {
 				bodyResponse.setCodigoRespuesta(Constante.CODIGO.IDF1_VALIDAR_CODIGO_CANTIDAD_INTENTOS_SUPERADO);
 				bodyResponse.setMensajeRespuesta(constante.IDF1_VALIDAR_CODIGO_CANTIDAD_INTENTOS_SUPERADO());
 				estadoHttp = HttpStatus.CREATED;
-				log.info(METHOD_NAME + bodyResponse.getMensajeRespuesta());
+				log.info(METHOD_NAME , bodyResponse.getMensajeRespuesta());
 			} else if (Constante.CODIGO.IDF4_VALIDAR_CODIGO_NO_EXITO.equalsIgnoreCase(codigoValidado.getCodigo())) {
 				bodyResponse.setCodigoRespuesta(codigoValidado.getCodigo());
 				bodyResponse.setMensajeRespuesta(constante.IDF4_VALIDAR_CODIGO_NO_EXITO());
 				estadoHttp = HttpStatus.CREATED;
-				log.info(METHOD_NAME + bodyResponse.getMensajeRespuesta());
+				log.info(METHOD_NAME , bodyResponse.getMensajeRespuesta());
 			} else if (Constante.CODIGO.IDT1_VALIDAR_CODIGO_ERROR.equalsIgnoreCase(codigoValidado.getCodigo())) {
 				bodyResponse.setCodigoRespuesta(codigoValidado.getCodigo());
 				bodyResponse.setMensajeRespuesta(constante.IDT1_VALIDAR_CODIGO_ERROR());
 				estadoHttp = HttpStatus.INTERNAL_SERVER_ERROR;
-				log.info(METHOD_NAME + bodyResponse.getMensajeRespuesta());
+				log.info(METHOD_NAME , bodyResponse.getMensajeRespuesta());
 			}
-			log.info(METHOD_NAME + "Termina API REST.");
+			log.info(METHOD_NAME , "Termina API REST.");
 		} catch (Exception e) {
-			log.error(constante.IDT1_VALIDAR_CODIGO_ERROR() + e.getMessage());
+			log.error(constante.IDT1_VALIDAR_CODIGO_ERROR() , e.getMessage());
 			bodyResponse.setCodigoRespuesta(Constante.CODIGO.IDT1_VALIDAR_CODIGO_ERROR);
 			bodyResponse.setMensajeRespuesta(constante.IDT1_VALIDAR_CODIGO_ERROR());
 			estadoHttp = HttpStatus.INTERNAL_SERVER_ERROR;
