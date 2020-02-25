e056db3927562ee0771a9569c8ac2fb21869b701
jcisneros-avatar <jcisneros@avatar-global.com>
2020-02-25 17:36:35 -0500

    mllamocca_250220: se agregan dependencias para el trabajo del front en la api rest claro

--- ServiceRESTConsultarStockOnlineClaroAvatar/src/main/java/pe/com/claro/ServiceRESTConsultarStockOnlineClaroAvatar.java
+++ ServiceRESTConsultarStockOnlineClaroAvatar/src/main/java/pe/com/claro/ServiceRESTConsultarStockOnlineClaroAvatar.java
@@ -4,11 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class ServiceRESTConsultarStockOnlineClaroAvatar extends SpringBootServletInitializer {
 
 	private static Class<ServiceRESTConsultarStockOnlineClaroAvatar> applicationClass = ServiceRESTConsultarStockOnlineClaroAvatar.class;
@@ -20,11 +17,6 @@ public class ServiceRESTConsultarStockOnlineClaroAvatar extends SpringBootServle
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(applicationClass);
-	}
-
-	@GetMapping(value = "/")
-	public String init() {
-		return "Services REST SISACT Avatar: ServiceRESTConsultarStockOnlineClaroAvatar";
 	}
 
 }
